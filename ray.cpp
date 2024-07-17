c16a1e7991726d00569738988681a59349710e2a
suhayel1 <141796181+suhayel1@users.noreply.github.com>
2024-07-16 20:26:48 -0400

    Create ray.cpp

---
+++ ray.cpp
@@ -0,0 +1,13 @@
+#include "ray.h"
+
+Ray::Ray() {}
+
+// overloaded constructor definitions here
+
+void Ray::incDepth() {
+    // increase depth of ray
+}
+
+void Ray::showDetails() {
+    // output details of Ray obj instance
+}
