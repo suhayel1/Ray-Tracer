5f751fe9739c8ebf7a32ae55a1a4ecd182acaadf
suhayel1 <141796181+suhayel1@users.noreply.github.com>
2024-07-16 20:16:56 -0400

    Create camera.h

---
+++ camera.h
@@ -0,0 +1,18 @@
+#ifndef CAMERA_H
+#define CAMERA
+
+#include <iostream>
+#include <string>
+
+#include "glm/glm.hpp"
+
+class Camera {
+public:
+    // variables
+
+    // constructor
+
+    // function that shows details of Camera object
+};
+
+#endif
