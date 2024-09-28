a28dca4612f6fbd94b201a10e075f7999dd87e41
suhayel1 <141796181+suhayel1@users.noreply.github.com>
2024-09-27 20:28:50 -0400

    Update light.cpp

--- light.cpp
+++ light.cpp
@@ -1,9 +1,18 @@
 #include "light.h"
 
-Light::Light() {
-    // initialize variables
+Light::Light(std::string name, float pos[3], float iRgb[3]) {
+    this->name = name;
+
+    for (int i = 0; i < 3; i++) {
+        this->pos[i] = pos[i];
+        this->iRgb[i] = iRgb[i];
+    }
 }
 
 void Light::showDetails() {
-    // show details of Light obj
+    std::cout << "LIGHT" << std::endl;
+    std::cout << "=====" << std::endl;
+    std::cout << "Name: " << this->name << std::endl;
+    std::cout << "Position: (" << this->pos[0] << ", " << this->pos[1] << ", " << this->pos[2] << ")" << std::endl;
+    std::cout << "Intensity (RGB): (" << this->iRgb[0] << ", " << this->iRgb[1] << ", " << this->iRgb[2] << ")\n" << std::endl;
 }
