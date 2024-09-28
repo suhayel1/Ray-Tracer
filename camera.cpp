4c9b6c10283ed5d969884ef474a7e9bd85ce006a
suhayel1 <141796181+suhayel1@users.noreply.github.com>
2024-09-27 20:28:14 -0400

    Update camera.cpp

--- camera.cpp
+++ camera.cpp
@@ -1,9 +1,24 @@
 #include "camera.h"
 
 Camera::Camera() {  // right-handed
-    // set camera at origin
+    this->eye = glm::vec3(0.0f);
+    this->u = glm::vec3(1.0f, 0.0f, 0.0f);
+    this->v = glm::vec3(0.0f, 1.0f, 0.0f);
+    this->n = glm::vec3(0.0f, 0.0f, 1.0f);
+}
+
+void Camera::showVec(glm::vec3 v) {
+    std::cout << "(" << v.x << ", " << v.y << ", " << v.z << ")" << std::endl;
 }
 
 void Camera::showDetails() {
-    // show variable values of Cam obj
+    std::cout << "EYE: ";
+    Camera::showVec(this->eye);
+    std::cout << "U: ";
+    Camera::showVec(this->u);
+    std::cout << "V: ";
+    Camera::showVec(this->v);
+    std::cout << "N: ";
+    Camera::showVec(this->n);
+    std::cout << std::endl;
 }
