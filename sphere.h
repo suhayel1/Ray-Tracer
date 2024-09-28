1378fc536c854ecf3d5f00753ba08cd5e1d3e7a6
suhayel1 <141796181+suhayel1@users.noreply.github.com>
2024-09-27 20:29:45 -0400

    Update sphere.h

--- sphere.h
+++ sphere.h
@@ -6,9 +6,15 @@
 
 class Sphere {
 public:
-    // variables
+    std::string name;
+    float pos[3];
+    float scl[3];
+    float rgb[3];
+    float k[4]; // [Ka, Kd, Ks, Kr]
+    int n;
 
     Sphere();
+    Sphere(std::string name, float pos[3], float scl[3], float rgb[3], float k[4], int n);
 
     void showDetails();
 };
